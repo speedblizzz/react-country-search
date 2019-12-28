f5bdc600fd4daae3460d41c4cfaac74957a7c2f8
Sergey Ponomarev <speedblizzz@gmail.com>
2019-12-28 22:52:46 +0500

    prepared project structure, created App and AppHeader components, added node-sass package

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './AppHeader.scss'
+
+export default class AppHeader extends Component {
+  render() {
+    return <h1 className="main-title">App Title</h1>
+  }
+}
